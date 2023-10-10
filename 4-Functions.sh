1eb434bde58e57bc0970454bb42852265269dcca
Venktesh Sharma <svenktesh77@gmail.com>
2023-10-10 14:28:10 -0500

    learning 1

--- 4-Functions.sh
+++ 4-Functions.sh
@@ -1,13 +1,12 @@
 # Declare a function
 abc() {
   echo -e "\e[32mHello World\e[0m"
-
-}
-#How to access a function
-abc
-
  if [0.5 -gt 5]; then
    echo -e " \e[32m1\e[0m"
 else
     echo -e " \e[32m0.5\e[0m"
 fi
+}
+#How to access a function
+abc
+
