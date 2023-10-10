55cd35db2bd1ac17f0d6807c87499e0cc2dd6f0e
Venktesh Sharma <svenktesh77@gmail.com>
2023-10-10 14:43:46 -0500

    learning 1

--- 4-Functions.sh
+++ 4-Functions.sh
@@ -1,10 +1,9 @@
 # Declare a function
-abc() {
-  echo -e "\e[32mHello World\e[0m"
+
 if [ $1 -lt $5 ]; then
-   echo -e " \e[32m$1\e[0m"
+   echo -e " \e[32m$1\e[Om"
 else
-    echo -e " \e[32m$5\e[0m"
+    echo -e " \e[32m$5\e[Om"
 fi
 }
 #How to access a function
